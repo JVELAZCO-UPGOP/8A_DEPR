acc53b8453df16393c1babcbcf4079a8d78c5562
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-01 20:09:18 -0600

    Segundo commit-VeterinariaEvelinModificacionesModal

--- js/duenos.js
+++ js/duenos.js
@@ -1,11 +1,13 @@
-const listaDuenos=document.getElementById('lista-duenos');
-const pais=document.getElementById('pais');
-const identificacion=document.getElementById('identificacion');
-const nombre=document.getElementById('nombre');
-const apellido= document.getElementById('apellido');
-const form= document.getElementById('form');
-const indice= document.getElementById('indice');
-const btnGuardar=document.getElementById('btn-guardar');
+const listaDuenos = document.getElementById('lista-duenos');
+const pais = document.getElementById('pais');
+const nombre = document.getElementById('nombre');
+const identificacion = document.getElementById('identificacion');
+const apellido = document.getElementById('apellido');
+const form = document.getElementById('form');
+const btnGuardar = document.getElementById('btn-guardar');
+const indice = document.getElementById('indice');
+const titulo = document.getElementById('exampleModalCenterTitle');
+const modal = document.getElementById('exampleModalCenter');
 
 
 let duenos=[
@@ -25,80 +27,136 @@ let duenos=[
 ];
 
 function listarDuenos() {
-    const htmlDuenos =duenos.map((dueno, index)=>`<tr>
-    <th scope="row">${index}</th>
-    <td>${dueno.identificacion}</td>
-    <td>${dueno.pais}</td>
-    <td>${dueno.nombre}</td>
-    <td>${dueno.apellido}</td>
-    <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-           <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
-           <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
-          
-      </div>
-    </td>
-  </tr>`).join("");
-  listaDuenos.innerHTML = htmlDuenos;
- Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=> botonEditar.onclick= editar(index));
- Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=> botonEliminar.onclick= eliminar(index));
-}
-
-function enviarDatos(evento) {
+    const htmlDuenos = duenos.map((dueno, index)=>`<tr>
+        <th scope="row">${index}</th>
+        <td>${dueno.identificacion}</td>
+        <td>${dueno.pais}</td>
+        <td>${dueno.nombre}</td>
+        <td>${dueno.apellido}</td>
+        <td>
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
+                <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
+            </div>
+        </td>
+      </tr>`).join("");
+      listaDuenos.innerHTML = htmlDuenos;
+      Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
+      Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
+  }
+  
+  function enviarDatos(evento) {
     evento.preventDefault();
     const datos = {
-        nombre: nombre.value,
-         apellido: apellido.value,
-         pais: pais.value,
-         identificacion: identificacion.value                                                                                                      
+      nombre: nombre.value,
+      apellido: apellido.value,
+      pais: pais.value,
+      identificacion: identificacion.value
     };
     const accion = btnGuardar.innerHTML;
-    switch(accion){
-        case 'Editar':
-            //Editar
-            duenos[indice.value] = datos;
-            break;
-            default:
-                //crear
-                duenos.push(datos);
-                break;
+    switch(accion) {
+      case 'Editar':
+        duenos[indice.value] = datos;
+        break;
+      default:
+        duenos.push(datos);
+        break;
     }
- listarDuenos();
- resetModal();
-}
-function editar(index){
-    return function cuandoHagaClick (){
-    btnGuardar.innerHTML = 'Editar'
-    $('#exampleModalCenter').modal('toggle');
-    const dueno = duenos[index];
-    nombre.value = dueno.nombre;
-    apellido.value = dueno.apellido;
-    pais.value = dueno.pais;
-    identificacion.value = dueno.tipo;
-    indice.value = index;
-    
-   }
-}
+    listarDuenos();
+    resetModal();
+  }
+  
+  function editar(index) {
+    return function cuandoCliqueo() {
+      btnGuardar.innerHTML = 'Editar'
+      titulo.innerHTML = "Editar Dueño"
+      $('#exampleModalCenter').modal('toggle');
+      const dueno = duenos[index];
+      indice.value = index;
+      nombre.value = dueno.nombre;
+      apellido.value = dueno.apellido;
+      pais.value = dueno.pais;
+      identificacion.value = dueno.identificacion;
 
-function resetModal(){
+
+      $("#btn-cerrar").on("click",function() {
+        indice.value = '';
+        nombre.value = '';
+        apellido.value = '';
+        pais.value = 'País';
+        identificacion.value = '';
+        btnGuardar.innerHTML = 'Crear'
+        titulo.innerHTML = "Nuevo Dueño";
+     });
+
+    $("#btn-close1").on("click",function() {
+      indice.value = '';
+      nombre.value = '';
+      apellido.value = '';
+      pais.value = 'País';
+      identificacion.value = '';
+      btnGuardar.innerHTML = 'Crear'
+        titulo.innerHTML = "Nuevo Dueño";
+        });
+    }
+  }
+  
+  function resetModal() {
+    indice.value = '';
     nombre.value = '';
     apellido.value = '';
-    pais.value = '';
+    pais.value = 'País';
     identificacion.value = '';
-    indice.value ='';
-    btnGuardar.innerHTML ='Crear'
-}
-function eliminar(index){
-    return function clickenEliminar(){
-        
-        duenos = duenos.filter((dueno, indicedueno)=>indicedueno !==index);
-        listarDuenos();
-    }
+    btnGuardar.innerHTML = 'Crear'
+  }
+  
+  function eliminar(index) {
    
+    return function clickEnEliminar() {
+      $('#exampleModalCenter2').modal('toggle');
+      const dueno = duenos[index];
+      indice.value = index;
+      nombre.value = dueno.nombre;
+      apellido.value = dueno.apellido;
+      pais.value = dueno.pais;
+      identificacion.value = dueno.identificacion;
 
-}
+      $("#btn-eliminar2").on("click",function() {
+        duenos = duenos.filter((dueno, indice)=>indice !== index);
+        listarDuenos();
+        indice.value = '';
+        nombre.value = '';
+        apellido.value = '';
+        pais.value = 'País';
+        identificacion.value = '';
+        titulo.innerHTML = "Nuevo Dueño";
+        btnGuardar.innerHTML = 'Guardar';
+        });
 
-listarDuenos();
+        $("#btn-cerrar2").on("click",function() {
+           indice.value = '';
+           nombre.value = '';
+           apellido.value = '';
+           pais.value = 'País';
+           identificacion.value = '';
+          titulo.innerHTML = "Nuevo Dueño";
+          btnGuardar.innerHTML = 'Guardar';
+          });
 
-form.onsubmit = enviarDatos;
-btnGuardar.onclick= enviarDatos;
+          $("#btn-cerrar5").on("click",function() {
+            indice.value = '';
+            nombre.value = '';
+            apellido.value = '';
+            pais.value = 'País';
+            identificacion.value = '';
+            btnGuardar.innerHTML = 'Crear'
+              titulo.innerHTML = "Nuevo Dueño";
+              });
+          }
+      }
+    
+  
+  listarDuenos();
+  
+  form.onsubmit = enviarDatos;
+  btnGuardar.onclick = enviarDatos;
--- js/veterinarios.js
+++ js/veterinarios.js
@@ -1,11 +1,13 @@
-const listaVeterinarios=document.getElementById('lista-veterinarios');
-const pais=document.getElementById('pais');
-const identificacion=document.getElementById('identificacion');
-const nombre=document.getElementById('nombre');
-const apellido= document.getElementById('apellido');
-const form= document.getElementById('form');
-const indice= document.getElementById('indice');
-const btnGuardar=document.getElementById('btn-guardar');
+const listaVeterinarios = document.getElementById('lista-veterinarios');
+const pais = document.getElementById('pais');
+const nombre = document.getElementById('nombre');
+const identificacion = document.getElementById('identificacion');
+const apellido = document.getElementById('apellido');
+const form = document.getElementById('form');
+const btnGuardar = document.getElementById('btn-guardar');
+const indice = document.getElementById('indice');
+const titulo = document.getElementById('exampleModalCenterTitle');
+const modal = document.getElementById('exampleModalCenter');
 
 
 let veterinarios=[
@@ -24,81 +26,139 @@ let veterinarios=[
     }
 ];
 
-function listarVeterinarios() {
-    const htmVeterinarios =veterinarios.map((veterinario, index)=>`<tr>
-    <th scope="row">${index}</th>
-    <td>${veterinario.identificacion}</td>
-    <td>${veterinario.pais}</td>
-    <td>${veterinario.nombre}</td>
-    <td>${veterinario.apellido}</td>
-    <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-           <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
-           <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
-          
-      </div>
-    </td>
-  </tr>`).join("");
-  listaVeterinarios.innerHTML = htmVeterinarios;
- Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=> botonEditar.onclick= editar(index));
- Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=> botonEliminar.onclick= eliminar(index));
-}
 
-function enviarDatos(evento) {
+function listarVeterinarios() {
+    const htmlVeterinarios = veterinarios.map((veterinario, index)=>`<tr>
+        <th scope="row">${index}</th>
+        <td>${veterinario.identificacion}</td>
+        <td>${veterinario.pais}</td>
+        <td>${veterinario.nombre}</td>
+        <td>${veterinario.apellido}</td>
+        <td>
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
+                <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
+            </div>
+        </td>
+      </tr>`).join("");
+      listaVeterinarios.innerHTML = htmlVeterinarios;
+      Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
+      Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
+  }
+  
+  function enviarDatos(evento) {
     evento.preventDefault();
     const datos = {
-        nombre: nombre.value,
-         apellido: apellido.value,
-         pais: pais.value,
-         identificacion: identificacion.value                                                                                                      
+      nombre: nombre.value,
+      apellido: apellido.value,
+      pais: pais.value,
+      identificacion: identificacion.value
     };
     const accion = btnGuardar.innerHTML;
-    switch(accion){
-        case 'Editar':
-            //Editar
-            veterinarios[indice.value] = datos;
-            break;
-            default:
-                //crear
-                veterinarios.push(datos);
-                break;
+    switch(accion) {
+      case 'Editar':
+        veterinarios[indice.value] = datos;
+        break;
+      default:
+        veterinarios.push(datos);
+        break;
     }
- listarVeterinarios();
- resetModal();
-}
-function editar(index){
-    return function cuandoHagaClick (){
-    btnGuardar.innerHTML = 'Editar'
-    $('#exampleModalCenter').modal('toggle');
-    const veterinario = veterinarios[index];
-    nombre.value = veterinario.nombre;
-    apellido.value = veterinario.apellido;
-    pais.value = veterinario.pais;
-    identificacion.value = veterinario.tipo;
-    indice.value = index;
-    
-   }
-}
+    listarVeterinarias();
+    resetModal();
+  }
+  
+  function editar(index) {
+    return function cuandoCliqueo() {
+      btnGuardar.innerHTML = 'Editar'
+      titulo.innerHTML = "Editar Veterinario";
+      $('#exampleModalCenter').modal('toggle');
+      const veterinario = veterinarios[index];
+      indice.value = index;
+      nombre.value = veterinario.nombre;
+      apellido.value = veterinario.apellido;
+      pais.value = veterinario.pais;
+      identificacion.value = veterinario.identificacion;
 
-function resetModal(){
+      $("#btn-cerrar").on("click",function() {
+        indice.value = '';
+        nombre.value = '';
+        apellido.value = '';
+        pais.value = 'País';
+        identificacion.value = 'Identificacion';
+        btnGuardar.innerHTML = 'Crear'
+        titulo.innerHTML = "Nueva Veterinario";
+     });
+
+    $("#btn-close1").on("click",function() {
+      indice.value = '';
+      nombre.value = '';
+      apellido.value = '';
+      pais.value = 'País';
+      identificacion.value = 'Identificacion';
+      btnGuardar.innerHTML = 'Crear'
+      titulo.innerHTML = "Nueva Veterinario";
+        });
+
+    }
+
+  }
+  
+  function resetModal() {
+    indice.value = '';
     nombre.value = '';
     apellido.value = '';
-    pais.value = '';
+    pais.value = 'País';
     identificacion.value = '';
-    indice.value ='';
-    btnGuardar.innerHTML ='Crear'
-}
-function eliminar(index){
-    return function clickenEliminar(){
-        
-        veterinarios = veterinarios.filter((veterinario, indiceveterinario)=>indiceveterinario !==index);
+    btnGuardar.innerHTML = 'Crear'
+  }
+  
+  function eliminar(index) {
+
+    return function clickEnEliminar() {
+      $('#exampleModalCenter2').modal('toggle');
+      const veterinario = veterinarios[index];
+      indice.value = index;
+      nombre.value = veterinario.nombre;
+      apellido.value = veterinario.apellido;
+      pais.value = veterinario.pais;
+      identificacion.value = veterinario.identificacion;
+
+      $("#btn-eliminar2").on("click",function() {
+        veterinarios = veterinarios.filter((veterinario, indice)=>indice !== index);
         listarVeterinarios();
-    }
-   
+        indice.value = '';
+        nombre.value = '';
+        apellido.value = '';
+        pais.value = 'País';
+        identificacion.value = '';
+        btnGuardar.innerHTML = 'Crear'
+        titulo.innerHTML = "Nueva Veterinario";
+        });
 
-}
+        $("#btn-cerrar2").on("click",function() {
+          indice.value = '';
+          nombre.value = '';
+          apellido.value = '';
+          pais.value = 'País';
+          identificacion.value = '';
+          btnGuardar.innerHTML = 'Crear'
+          titulo.innerHTML = "Nueva Veterinario";
+          });
 
-listarVeterinarios();
-
-form.onsubmit = enviarDatos;
-btnGuardar.onclick= enviarDatos;
+          $("#btn-cerrar5").on("click",function() {
+            indice.value = '';
+            nombre.value = '';
+            apellido.value = '';
+            pais.value = 'País';
+            identificacion.value = '';
+            btnGuardar.innerHTML = 'Crear'
+            titulo.innerHTML = "Nueva Veterinario";
+          });
+          
+      }
+  }
+  
+  listarVeterinarios();
+  
+  form.onsubmit = enviarDatos;
+  btnGuardar.onclick = enviarDatos;
