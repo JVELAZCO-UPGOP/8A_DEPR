3197ad0d161797e93c730fc18566feaad26d5788
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-05 15:34:01 -0600

    modulacion recursos y requestHandle

---
+++ nodejs/recursos.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mascotas: [
+        { tipo: "perro", nombre: "puka", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka", dueno: "Felix" }
+    ],
+};
