ad6968a6067139059bd1851bad632cb346432e88
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-05 15:57:20 -0600

    Actualizar mascotas

--- nodejs/Enrutador.js
+++ nodejs/Enrutador.js
@@ -20,6 +20,18 @@ module.exports = {
             callback(201, data.payload);
 
         },
+        put: (data, callback) => {
+            if (typeof data.indice !== "undefined") {
+                if (global.recursos.mascotas[data.indice]) {
+                    global.recursos.mascotas[data.indice] = data.payload;
+                    return callback(200, global.recursos.mascotas[data.indice]);
+                }
+                return callback(404, {
+                    mensaje: `Mascota con indice ${data.indice} no encontrado`,
+                });
+            }
+            callback(404, { mensaje: "Indice No Enviado" });
+        },
     },
     noEncontrado: (data, callback) => {
         callback(404, { mensaje: 'No encontrado' });
