75e3b944eed7a0a4f28a27122eca4be514f61fcc
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-06 16:21:44 -0600

    ruta/veterinarios

--- nodejs/Enrutador.js
+++ nodejs/Enrutador.js
@@ -1,11 +1,13 @@
 const recursos = require("./recursos");
 const mascotas = require("./rutas/mascotas");
+const veterinarios = require("./rutas/veterinario");
 
 module.exports = {
     ruta: (data, callback) => {
         callback(200, { mensaje: 'Esta es la /ruta' });
     },
     mascotas: mascotas(recursos.mascotas),
+    veterinarios: veterinarios(recursos.veterinarios),
     noEncontrado: (data, callback) => {
         callback(404, { mensaje: 'No encontrado' });
     },
--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -1,9 +1,17 @@
 module.exports = {
     mascotas: [
-        { tipo: "perro", nombre: "puka", dueno: "Felix" },
-        { tipo: "perro", nombre: "puka", dueno: "Felix" },
-        { tipo: "perro", nombre: "puka", dueno: "Felix" },
-        { tipo: "perro", nombre: "puka", dueno: "Felix" },
-        { tipo: "perro", nombre: "puka", dueno: "Felix" }
+        { tipo: "perro", nombre: "puka0", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka1", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka2", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka3", dueno: "Felix" },
+        { tipo: "perro", nombre: "puka4", dueno: "Felix" }
+    ],
+    veterinarios: [
+        { nombre: "Juan", apellido: "Perez", documento: "18080392" },
+        { nombre: "Maria", apellido: "Orona", documento: "11010091" },
+        { nombre: "Alvaro", apellido: "Solis", documento: "13092209" },
+        { nombre: "Maidelin", apellido: "Del Rio", documento: "19010299" },
+        { nombre: "Juan", apellido: "Perez", documento: "18080392" }
+
     ],
 };
