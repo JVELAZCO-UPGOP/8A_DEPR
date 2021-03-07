f56fef54245dbab5df43fa0a7cb43e6d7dfe2bfd
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-06 20:18:39 -0600

    mover carpeta

--- veterinaria-fullstack/backend/index.js
+++ veterinaria-fullstack/backend/index.js
@@ -2,6 +2,6 @@ const http = require('http');
 const requestHandle = require("./request-handle");
 const server = http.createServer(requestHandle);
 
-server.listen(5000, () => {
-    console.log('El servidor esta esta escuchando peticiones en http://localhost:5000/');
+server.listen(6000, () => {
+    console.log('El servidor esta esta escuchando peticiones en http://localhost:6000/');
 });
