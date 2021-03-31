ea503a1e2383ba31d210877555bae52e94b1a604
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-31 17:57:30 -0600

    Componente <ActionsMenu />

--- react-2/src/componentes/ActionsMenu/index.js
+++ react-2/src/componentes/ActionsMenu/index.js
@@ -3,23 +3,24 @@ import "./ActionsMenu.css";
 
 
 function ActionsMenu({ cambiarModal = () => {} }) {
-  return (
-    <div className="actions-menu">
-      <h1>Mascotas</h1>
-      <div className="actions-menu-content">
-        <button
-          type="button"
-          className="btn btn-primary"
-          data-toggle="modal"
-          data-target="#exampleModalCenter"
-          onClick={cambiarModal}
+    return ( <
+        div className = "actions-menu" >
+        <
+        h1 > Mascotas < /h1> <
+        div className = "actions-menu-content" >
+        <
+        button type = "button"
+        className = "btn btn-primary"
+        data - toggle = "modal"
+        data - target = "#exampleModalCenter"
+        onClick = { cambiarModal }
 
         >
-          Nueva
-        </button>
-      </div>
-    </div>
-  );
+        Nueva <
+        /button> <
+        /div> <
+        /div>
+    );
 }
 
 export default ActionsMenu;
