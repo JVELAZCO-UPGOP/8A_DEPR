b4a46e9216228f08dcffa7600cf3cbc342cb6096
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-31 16:14:42 -0600

    react-2

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Pagina from "./Pagina";
 
 function App() {
-
-    return <Pagina / >
+ 
+  return <Pagina />
 }
 
 export default App;
--- react-2/src/Pagina.js
+++ react-2/src/Pagina.js
@@ -6,36 +6,31 @@ import Modal from "./componentes/Modal";
 
 
 class Pagina extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mostraModal: false,
-        };
-    }
-
-    cambiarModal = () => {
-        this.setState({ mostraModal: !this.state.mostraModal });
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostraModal: false,
     };
+  }
 
-    //render siempre va al ultimo
-    render() {
-        return ( <
-            >
-            <
-            div className = "container" >
-            <
-            Nav / >
-            <
-            ActionsMenu cambiarModal = { this.cambiarModal }
-            /> <
-            Tabla / > {
-                this.state.mostraModal && < Modal cambiarModal = { this.cambiarModal }
-                />} <
-                /div> <
-                />
-            );
-        }
+  cambiarModal = () => {
+    this.setState({ mostraModal: !this.state.mostraModal });
+  };
 
-    }
+  //render siempre va al ultimo
+  render() {
+    return (
+      <>
+        <div className="container">
+          <Nav />
+          <ActionsMenu cambiarModal={this.cambiarModal} />
+          <Tabla />
+          {this.state.mostraModal && <Modal cambiarModal={this.cambiarModal} />}
+        </div>
+      </>
+    );
+  }
 
-    export default Pagina;
+}
+
+export default Pagina;
