7987b3f8571608674b0c95447633124651d5028a
EvelinPerez08 <77990338+EvelinPerez08@users.noreply.github.com>
2021-03-31 18:14:51 -0600

    Modal

--- react-2/src/componentes/Modal/index.js
+++ react-2/src/componentes/Modal/index.js
@@ -6,51 +6,69 @@ import Input from "../Input";
 import ModalFooter from "./ModalFooter";
 
 const tiposMascota = [
-  { valor: "Perro", etiqueta: "Perro" },
-  { valor: "Gato", etiqueta: "Gato" },
-  { valor: "Pájaro", etiqueta: "Pájaro" },
-  { valor: "Otro", etiqueta: "Otro" },
+    { valor: "Perro", etiqueta: "Perro" },
+    { valor: "Gato", etiqueta: "Gato" },
+    { valor: "Pájaro", etiqueta: "Pájaro" },
+    { valor: "Otro", etiqueta: "Otro" },
 ];
 
 const duenos = [
-  { valor: "Miguel", etiqueta: "Miguel" },
-  { valor: "Susan", etiqueta: "Susan" },
-  { valor: "Mario", etiqueta: "Mario" },
-  { valor: "Jose", etiqueta: "Jose" },
-  { valor: "Karen", etiqueta: "Karen" },
+    { valor: "Claudia", etiqueta: "Claudia" },
+    { valor: "Favio", etiqueta: "Favio" },
+    { valor: "Jesus", etiqueta: "Jesus" },
+    { valor: "Celia", etiqueta: "Celia" },
 ];
 
 function Modal({ cambiarModal = () => {} }) {
-  return (
-    <>
-    <div className="modal">
-    <div className="modal-dialog modal-dialog-centered">
-      <div className="modal-content">
-      <ModalHeader cambiarModal={cambiarModal} />
-        <div className="modal-body">
-          <form id="form">
-          <div className="form-row">
-                  <div className="col">
-                    <Select options={tiposMascota} nombreCampo="Tipo animal" />
-                  </div>
-          </div>
-            <div className="form-row">
-              <div className="col">
-              <Input tipo="text" nombreCampo="nombre" />
-              </div>
-              <div className="col">
-              <Select options={duenos} nombreCampo="dueño" />
-              </div>
-            </div>
-            </form>
-            </div>
-            <ModalFooter cambiarModal={cambiarModal} />
-          </div>
-        </div>
-    </div>
-      <div class="modal-backdrop fade show"></div>
-    </>
-  );
+    return ( <
+        >
+        <
+        div className = "modal" >
+        <
+        div className = "modal-dialog modal-dialog-centered" >
+        <
+        div className = "modal-content" >
+        <
+        ModalHeader cambiarModal = { cambiarModal }
+        /> <
+        div className = "modal-body" >
+        <
+        form id = "form" >
+        <
+        div className = "form-row" >
+        <
+        div className = "col" >
+        <
+        Select options = { tiposMascota }
+        nombreCampo = "Tipo animal" / >
+        <
+        /div> <
+        /div> <
+        div className = "form-row" >
+        <
+        div className = "col" >
+        <
+        Input tipo = "text"
+        nombreCampo = "nombre" / >
+        <
+        /div> <
+        div className = "col" >
+        <
+        Select options = { duenos }
+        nombreCampo = "dueño" / >
+        <
+        /div> <
+        /div> <
+        /form> <
+        /div> <
+        ModalFooter cambiarModal = { cambiarModal }
+        /> <
+        /div> <
+        /div> <
+        /div> <
+        div class = "modal-backdrop fade show" > < /div> <
+        />
+    );
 }
 
 export default Modal;
